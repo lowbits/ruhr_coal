642938355b99966b378ce88711c4b9f51b58218e
Simon Kubiak <mail@simonkubiak.de>
2017-11-18 17:27:09 +0100

    Relationships für Activity und Location hinzugefügt

--- app/Activity.php
+++ app/Activity.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function activities()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
