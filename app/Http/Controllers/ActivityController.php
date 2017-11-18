33ee81fb71d103d319abbcd07d482e5cf3c5967b
Tobias L <tobias.lobitz@stud.uni-due.de>
2017-11-18 23:36:34 +0100

    alpha entfernt

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -64,24 +64,12 @@ class ActivityController extends Controller
     {
 
         $this->validate($request, [
-            'title' => 'required|alpha',
+            'title' => 'required',
             'user_id' => 'required|exists:users,id',
             'location_id' => 'required|exists:locations,id'
         ]);
 
-        $activity->update([
-            'title' => request('title'),
-            'description' => request('description'),
-            'weather' => request('channel_id'),
-            'user_id' => auth()->id(),
-            'opening_hours' => request('opening_hours'),
-            'price' => request('price'),
-            'student_discount' => request('student_discount'),
-            'location_id' => request('location_id'),
-            'person_count' => request('person_count'),
-            'is_public' => request('is_public'),
-            'category' => request('category')
-        ]);
+        $activity->update($request->all());
     }
 
     /**
