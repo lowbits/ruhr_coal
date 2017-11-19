936b7cc47074a1eea1704444aefe4ff653ed04c0
Florian Köpke <git@floriankoepke.de>
2017-11-19 07:46:30 +0100

    AddActivity Button CSS

--- resources/assets/js/components/activities/addNewActivityButton.vue
+++ resources/assets/js/components/activities/addNewActivityButton.vue
@@ -69,6 +69,7 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
 
         span{
             color: #fff;
