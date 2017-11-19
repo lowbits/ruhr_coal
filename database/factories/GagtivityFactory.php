9f73e0cd83f1d1255ca77a9e84124c88ad905b1f
Simon Kubiak <mail@simonkubiak.de>
2017-11-19 10:04:32 +0100

    Kategorien

--- database/factories/GagtivityFactory.php
+++ database/factories/GagtivityFactory.php
@@ -9,14 +9,14 @@ $factory->define(Gactivity::class, function (Faker $faker) {
         'title' => $faker->sentence(3),
         'description' => $faker->sentence(),
         'user_id' => factory('App\User')->create()->id,
-        'price' => mt_rand() / mt_getrandmax() * random_int(10, 50),
+        'price' => random_int(2, 50),
         'location_id' => factory('App\Location')->create()->id,
         'min_person_count' => $pcount = random_int(1, 10),
         'max_person_count' => $pcount + random_int(1, 20),
         'is_public' => random_int(0, 1),
         'rhythm' => collect(['wöchentlich', 'einmalig', 'zweiwöchentlich'])->random(),
         'date' => Carbon::now()->addDay(10),
-        'category' => $faker->word,
+        'category' => 'Führung',
         'weather' => collect([
             'bei trockenem Wetter', 'bei jedem Wetter', 'ab 42°C mit Sandalen und Socken'
         ])->random(),
